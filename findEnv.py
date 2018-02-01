34b8ead2821a94975f60eb298fd8d7d3919d73a2
arturoalcibia <arturoalcibia@hotmail.com>
2018-02-01 15:49:58 -0600

    removed Scripts from keyword to find envVar

--- findEnv.py
+++ findEnv.py
@@ -2,18 +2,21 @@
 #arturoalcibia@hotmail.com
 #finds an specific path from environment vars
 import os
-def findEnv_(envWord = 'MAYA_SCRIPT_PATH', keyWordSection = 'Scripts', keywordOne = 'MKF', keywordTwo = 'RnD'):
+def findEnvVar_(envWord = 'MAYA_SCRIPT_PATH', keyWordSection = 'Scripts', keywordOne = 'MKF', keywordTwo = 'RnD'):
+	
 	path = ''
 	try:
-		envVar =os.environ[envWord]
+		envVar = os.environ[envWord]
 	except KeyError:
 		return None
 	envVar = envVar.split(';')
 
 	for x in envVar:
-		if x.find(keyWordSection) != -1:
-			if x.find(keywordOne) != -1 or x.find(keywordTwo):
-				path =  x
+		#if x.find(keyWordSection) != -1:
+		if x.find(keywordOne) != -1 or x.find(keywordTwo) != -1:
+			path =  x
 
 	return path
+
+print findEnvVar_()
 
