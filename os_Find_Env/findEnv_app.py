d4fa3f99e75f1a813d86a5f8d79089aa41da81df
arturoalcibia <arturoalcibia@hotmail.com>
2018-02-09 13:34:05 -0600

    print test

--- os_Find_Env/findEnv_app.py
+++ os_Find_Env/findEnv_app.py
@@ -12,6 +12,7 @@ def findEnvVar_(envWord = 'MAYA_SCRIPT_PATH', keyWordSection = 'Scripts', keywor
 	envVar = envVar.split(';')
 
 	for x in envVar:
+		print x
 		#if x.find(keyWordSection) != -1:
 		if x.find(keywordOne) != -1 or x.find(keywordTwo) != -1:
 			path =  x
