08455c501155030d427f8824bb4fbf3ccb7e2a9d
arturoalcibia <arturoalcibia@hotmail.com>
2018-02-10 12:25:56 -0600

    replaced to mel for env vars

--- os_Find_Env/findEnv_app.py
+++ os_Find_Env/findEnv_app.py
@@ -2,13 +2,11 @@
 #arturoalcibia@hotmail.com
 #finds an specific path from environment vars
 import os
+import maya.mel as mel
 def findEnvVar_(envWord = 'MAYA_SCRIPT_PATH', keyWordSection = 'Scripts', keywordOne = 'MKF', keywordTwo = 'RnD'):
 	
 	path = ''
-	try:
-		envVar = os.environ[envWord]
-	except KeyError:
-		return None
+	envVar = mel.eval('getenv "MAYA_SCRIPT_PATH";')
 	envVar = envVar.split(';')
 
 	for x in envVar:
